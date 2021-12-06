efe8798ee18d9621bb669cdc80a421c163a7137b
SnuggleTrouble <stabjo@outlook.com>
2021-12-06 14:12:44 +0100

    link CSS & JC to Index.html. Complete logic in memory.js

--- js/memory.js
+++ js/memory.js
@@ -2,20 +2,42 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
-  }
+    if (this.cards === undefined) {
+      return undefined;
+    } else {
+      for (let i = 0; i < this.cards.length; i++) {
+        let k = Math.floor(Math.random() * (this.cards.length));
+        let temporary = this.cards[i];
+        this.cards[i] = this.cards[k];
+        this.cards[k] = temporary;
+      }
+    }
+  };
 
   checkIfPair(card1, card2) {
-    // ... write your code here
-  }
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   checkIfFinished() {
-    // ... write your code here
-  }
-}
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
+  };;
+};
 
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
